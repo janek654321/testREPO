875c6e0480d6ebc5586a91c481aa68a9109027c4
jw <janwojcik2004@gmail.com>
2026-03-03 12:14:05 +0100

    Create GameRegistry.h

---
+++ GameRegistry.h
@@ -0,0 +1,24 @@
+#ifndef GAME_REGISTRY_H
+#define GAME_REGISTRY_H
+#include "Engine.h"
+
+#include "DemoGame.h"
+#include "Tetris.h"
+#include "Connect4.h"
+
+#define ADDGAME(GAME_CLASS, GAME_NAME) { GAME_NAME, []() -> Game* { return new GAME_CLASS(); } },
+typedef Game* (*GameClass)();
+
+struct GameEntry {
+  const char* const title;
+  const GameClass gameObj;
+};
+
+const GameEntry GAMES_LIST[] = {
+    ADDGAME(Tetris, "TETRIS")
+    ADDGAME(Connect4, "CONNECT 4")
+    ADDGAME(DemoGame, "DEMO")
+};
+
+const uint8_t TOTAL_GAMES_COUNT = sizeof(GAMES_LIST) / sizeof(GAMES_LIST[0]);
+#endif
